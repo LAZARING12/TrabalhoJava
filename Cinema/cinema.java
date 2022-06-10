6786879153dc79f879a0c97c0a9f23fe65274b40
Marcelo HAO <89233668+MAlbertuni@users.noreply.github.com>
2022-06-10 20:01:35 -0300

    da

--- Cinema/cinema.java
+++ Cinema/cinema.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class cinema {
+
+    public static void printMenu(String[] options){
+        
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
